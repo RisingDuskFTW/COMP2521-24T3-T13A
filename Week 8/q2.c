c212cb64379fd2226910d224f692b60973f5f624
Ryan Berlee <z5358674@ad.unsw.edu.au>
2024-10-30 19:56:10 +1100

    week 8 tutorial

--- Week 8/q2.c
+++ Week 8/q2.c
@@ -4,18 +4,42 @@
 
 // Solution 1: Using a BFS
 Set reachableBFS(Graph g, Vertex src) {
-	// TODO
-	return NULL;
+	Set seen = SetNew();
+	Queue q = QueueNew();
+
+	SetAdd(seen, src);
+	QueueEnqueue(q, src);
+	while (!QueueIsEmpty(q)) {
+		Vertex v = QueueDequeue(q);
+
+		// try all vertices
+		for (Vertex w = 0; w < GraphNumVertices(g); w++) {
+			// check if neighbour and not visited
+			if (v != w && GraphIsAdjacent(g, v, w) && !SetContains(seen, w)) {
+				QueueEnqueue(q, w);
+				SetAdd(seen, w);
+			}
+		}
+	}
+	return seen;
 }
 
 static void doReachable(Graph g, Vertex v, Set seen);
 
 // Solution 2: Using Recursive DFS
 Set reachableDFS(Graph g, Vertex src) {
-	// TODO
-	return NULL;
+	Set seen = SetNew();
+	doReachable(g, src, seen);
+	return seen;
 }
 
 static void doReachable(Graph g, Vertex v, Set seen) {
-	// TODO
+	// try all vertices
+	for (Vertex w = 0; w < GraphNumVertices(g); w++) {
+		// check if neighbour and not visited
+		if (v != w && GraphIsAdjacent(g, v, w) && !SetContains(seen, w)) {
+			doReachable(g, w, seen);
+			SetAdd(seen, w);
+		}
+	}
 }
