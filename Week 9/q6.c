132156c1713e97873a9a3fc3f22d1926487f2811
Ryan Berlee <z5358674@ad.unsw.edu.au>
2024-11-16 13:12:38 +1100

    week 9 tutorial

--- Week 9/q6.c
+++ Week 9/q6.c
@@ -3,8 +3,32 @@
 #include <stdbool.h>
 #include "HashTable.h"
 
+// Check if an array contains 2 integers that sum to S
+bool twoSum(int arr[], int size, int sum) {
+	HashTable ht = HashTableNew();
+	for (int i = 0; i < size; i++) {
+		if (HashTableContains(ht, sum - arr[i])) {
+			HashTableFree(ht);
+			return true;
+		}
+		HashTableInsert(ht, arr[i], 0);
+	}
+	HashTableFree(ht);
+	return false;
+}
+
 // Check if an array contains 3 integers that sum to S
 bool threeSum(int arr[], int size, int sum) {
-	// TODO
+	HashTable ht = HashTableNew();
+	for (int i = 0; i < size; i++) {
+		if (HashTableContains(ht, sum - arr[i])) {
+			HashTableFree(ht);
+			return true;
+		}
+		for (int j = 0; j < i; j++) {
+			HashTableInsert(ht, arr[i] + arr[j], 0);
+		}
+	}
+	HashTableFree(ht);
 	return false;
 }
